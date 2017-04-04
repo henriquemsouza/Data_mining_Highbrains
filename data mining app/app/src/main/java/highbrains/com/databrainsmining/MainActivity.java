5c1b1d5c577f1ae83a691efe0db46206b9b63766
Henrique Martins <henriquemsss@gmail.com>
2017-04-04 10:52:23 -0300

    update add lib twiter4j to project, top menu  and changed color of  the action bar

--- data mining app/app/src/main/java/highbrains/com/databrainsmining/MainActivity.java
+++ data mining app/app/src/main/java/highbrains/com/databrainsmining/MainActivity.java
@@ -1,12 +1,21 @@
 package highbrains.com.databrainsmining;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import twitter4j.Status;//lib from twitter4j
+import twitter4j.Twitter; //lib from twitter4j
+import twitter4j.TwitterFactory;//lib from twitter4j
+
 public class MainActivity extends AppCompatActivity {
+    AlertDialog.Builder dialog;
     Button btnlogin;
     Button btncadbtnloginastro;
     Button btntrends;
@@ -15,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         btnlogin=(Button) findViewById(R.id.btnLogin);
         btncadbtnloginastro=(Button) findViewById(R.id.btnCADASTRO);
         btntrends=(Button) findViewById(R.id.btnTrends);
@@ -32,4 +44,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    //
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_proximo:
+                // Toast.makeText(this, "Menu Item 1 selected", Toast.LENGTH_SHORT).show();
+                dialog=new AlertDialog.Builder(MainActivity.this);//CRIAR NOVO
+                dialog.setTitle("About");
+                dialog.setMessage("  Desenvolvido por \n From the best to the best\nHigh Brains @2017");
+                dialog.setIcon(android.R.drawable.ic_menu_info_details);
+                dialog.create();
+                dialog.show();
+
+
+                break;
+
+        }
+
+        return true;
+    }
+    //
+
+
+
+
 }
