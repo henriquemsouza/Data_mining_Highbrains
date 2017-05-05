9d752f0aaca49d2ad026f8cd1a20e41ac404fd7c
Aluno <aluno@band.net>
2017-05-05 20:20:14 -0300

    funcionando com  o banco

--- data mining app/app/src/main/java/highbrains/com/databrainsmining/TrendsActivity.java
+++ data mining app/app/src/main/java/highbrains/com/databrainsmining/TrendsActivity.java
@@ -4,16 +4,22 @@ import android.app.FragmentBreadCrumbs;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,16 +27,40 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TrendsActivity extends AppCompatActivity {
      public ListView ListaTRENDS;
     String[] list_command;
     FloatingActionButton FAB1;
     //String[] list_desc;
+//--------------galera way-----------------------------------------------------------------------------------
+    EditText genreName;
+    Button buttonBox;
 
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView lv;
+    ArrayList<HashMap<String, String>> genreList;
+    //--------------galera way-----------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trends);
+        //--------------galera way-----------------------------------------------------------------------------------
+        genreList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.ListView);
+
+        genreName = (EditText)findViewById(R.id.textBox);
+        buttonBox = (Button)findViewById(R.id.buttonBox);
+
+        new GetGenres().execute();
+        //--------------galera way-----------------------------------------------------------------------------------
+
         FAB1=(FloatingActionButton) findViewById(R.id.fbtNewSearch);
         FAB1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +82,7 @@ public class TrendsActivity extends AppCompatActivity {
                 new DataPoint(4, 6)
         });
         graph.addSeries(series);
-
+/*
         ListaTRENDS=(ListView) findViewById(R.id.ListView);
         //listView1=(ListView) findViewById(R.id.ListViewPage);
         Resources res=getResources();
@@ -80,20 +110,133 @@ public class TrendsActivity extends AppCompatActivity {
 
 
         });
+        */
     }
+    //-----------------------------------------------------------------------------------------------
+    private class GetGenres extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            Toast.makeText(TrendsActivity.this, "Json Data is downloading", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0)
+        {
+            HttpHandler sh = new HttpHandler();
+            String url = "http://aptwiapi.herokuapp.com/aptwi.json";
+            final String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null)
+            {
+                try
+                {
+                    final JSONObject jsonObj = new JSONObject(jsonStr);
+                    final JSONArray results = jsonObj.getJSONArray("results");
+                    for (int i = 0; i < results.length(); i++) {
+                        final JSONObject resultado = results.getJSONObject(i);
+                        final String id = resultado.getString("id");
+                        final String name = resultado.getString("name");
+
+                        HashMap<String, String> genre = new HashMap<>();
+                        genre.put("id", id);
+                        genre.put("name", name);
+
+                        genreList.add(genre);
+                    }
+                }
+                catch (final JSONException e)
+                {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "Json parsing error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+            else
+            {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Couldn't get json from server. Check LogCat" + jsonStr, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ListAdapter adapter = new SimpleAdapter(TrendsActivity.this, genreList, R.layout.list_item, new String[]{"id", "name"}, new int[]{R.id.email, R.id.mobile});
+            lv.setAdapter(adapter);
+        }
+    }
+
+    public  void executePost(View v)
+    {
+        String name  = genreName.getText().toString();
+
+        new PostGenre().execute(name);
+    }
+
+    private class PostGenre extends AsyncTask<String, Void, Void>
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            Toast.makeText(TrendsActivity.this, "Json Data is posting", Toast.LENGTH_LONG).show();
+        }
+        @Override
+        protected Void doInBackground(String... name)
+        {
+            HttpHandler sh = new HttpHandler();
+            String resposta = "";
+
+            String url = "http://aptwiapi.herokuapp.com/aptwi/";
+            try
+            {
+                resposta = sh.requestPost(url, name[0]);
+            }
+            catch (final Exception e)
+            {
+                Log.e(TAG, "Couldn't get json from server." + e.toString());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Couldn't get json from server. Check LogCat" + e.toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            super.onPostExecute(result);
+            Toast.makeText(TrendsActivity.this, "Json Data is finished", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------
 }
 
 class CustomAdapterPage extends ArrayAdapter<String>
 {
     Context context;
-  // String[] Descricao;
+  //String[] Descricao;
     String[] Comando;
 
     public CustomAdapterPage(Context context, String[] com)
     {
         super(context, R.layout.line_trends,com);
         this.context=context;
-
      // this.Descricao=descricao;
         this.Comando=com;
     }
@@ -102,17 +245,14 @@ class CustomAdapterPage extends ArrayAdapter<String>
     public View getView(int position, View convertView, ViewGroup parent)
     {
         LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
         View row=inflater.inflate(R.layout.line_trends, parent, false);
-
 
         TextView mycom= (TextView) row.findViewById(R.id.txtCom);
         TextView mydesc= (TextView) row.findViewById(R.id.txtDesc);
         mycom.setText(Comando[position]);
-      //  mydesc.setText(Descricao[position]);
+      //mydesc.setText(Descricao[position]);
 
         return row;
-
 
     }
 }
